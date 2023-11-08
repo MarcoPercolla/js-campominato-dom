c5697d32c5efc42712feca20b48011b2069d4323
ReDMallard <marco@titori.it>
2023-11-09 00:40:29 +0100

    aggiunta vittoria e counter

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
-var numCell = 64;
+let numCell = 64;
 let grid = document.getElementById("grid");
 const runBtn = document.getElementById("run");
 const difficultyInput = document.getElementById("difficulty");
-
-
+let safe = 0;
+// ---
 let bombe = [];
-while (bombe.length<16) {
-    let posizioneBomba = getRandom(1, numCell);
-    if (!(bombe.includes(posizioneBomba))) {
-        bombe.push(posizioneBomba);
-    }
+// while (bombe.length<16) {
+//     let posizioneBomba = getRandom(1, numCell);
+//     if (!(bombe.includes(posizioneBomba))) {
+//         bombe.push(posizioneBomba);
+//     }
     
-}
-console.log(bombe)
+// }
+// console.log(bombe)
 
-
+// ---
 function creaQuadrato(number) {
 
     const cell = document.createElement("div");
@@ -31,20 +31,30 @@ function creaQuadrato(number) {
     
 
     //evento click
+    
+    
     cell.addEventListener("click", function() {
-        
+          
         if (cell.classList.contains("bomb")) {
             grid.innerHTML= `<h2>Boom<h2>`;
+        } else if (safe == (numCell - 16)){
+            grid.innerHTML= `<h2>hai vinto<h2>`;
         } else {
-
             cell.classList.toggle("minato");
-            
-        } 
-
+            safe++;
+            document.getElementById("counter").innerHTML = `<h2>ti mancano ${(numCell - 16) - safe} celle</h2>`
+            console.log(safe)
+        }
+        
+        return safe;
     });
 
+    
+    
     return cell;
 }
+
+// ---
 
 function creaGriglia() {
 
@@ -64,13 +74,29 @@ function creaGriglia() {
     };
 }
 
+
+// ---
 runBtn.addEventListener("click", function () {
 
+    document.getElementById("counter").innerHTML = "";
     grid.innerHTML= "";
+    safe= "";
+    bombe = [];
+    
+    while (bombe.length<16) {
+        let posizioneBomba = getRandom(1, numCell);
+        if (!(bombe.includes(posizioneBomba))) {
+            bombe.push(posizioneBomba);
+        }
+        
+    }
+    console.log(bombe);
     creaGriglia();
 
 })
 
+
+// ---
 function getRandom(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
